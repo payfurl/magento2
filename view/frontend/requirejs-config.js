0e00a351b43f30239ae7062e94187beac3247f62
David Cameron <david@uberconcept.com>
2024-07-08 17:02:35 +1000

    new version

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -7,7 +7,7 @@
 let config = {
     map: {
         '*': {
-            'payfurljs': 'https://assets.payfurl.com/v4.6.5/js/payfurl.js'
+            'payfurljs': 'https://assets.payfurl.com/v4.6.9/js/payfurl.js'
         }
     }
 };
